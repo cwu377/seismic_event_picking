2934862f4e57db681c141a75729204d7e54cb7b3
Chao-sheng <cwu377@wisc.edu>
2023-10-26 18:11:08 -0500

    introduced squared CF (to fix occasionally near-to-zero LTA)

--- STA_LTA.py
+++ STA_LTA.py
@@ -2,17 +2,17 @@ import numpy as np
 
 def STA(data, N):
     data_short = data[(len(data)-N):len(data)]
-    return np.sum(data_short)/(N)
+    return np.divide( np.sum(np.square(data_short)),N )
 
-def LTA(data):
-    return np.sum(data)/len(data)
+def LTA(data, M):
+    return np.divide( np.sum(np.square(data)), M )
 
 def STA_LTA(data, M, N, threshold):
     for i in range(len(data)):
         data_temp = data[i:M+i]
         sta = STA(data_temp, N)
-        lta = LTA(data_temp)
-        R = sta/lta
+        lta = LTA(data_temp, M)
+        R = np.divide(sta,lta)
         if R > threshold:
             return i
         elif i==len(data)-1:
